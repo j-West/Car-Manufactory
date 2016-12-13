2de1b29d2569ed0d950b97a9aa7a19d2b08dd934
j-west <westja89@gmail.com>
2016-12-13 15:54:48 -0600

    Scott said this is bad

--- main.js
+++ main.js
@@ -3,6 +3,7 @@
   whose value will be Date.now()
  */
 function Car () {
+  this.manufacturedDate = Date.now();
   // Create the manufactured date property (see above)
 }
 
@@ -12,11 +13,12 @@ function Car () {
   3. Give it one property, named manufacturer, that holds a string value.
          For, example, this.manufacturer = "Ford".
  */
-function YourFavoriteMake () {
+function Honda () {
+  this.manufacturer = "Honda";
   // Define the manufacturer instance variable
 }
 // Set the prototype to a new Car instance
-
+Honda.prototype = new Car();
 
 /*
   1. Create a constructor function named after your favorite car model.
@@ -24,11 +26,14 @@ function YourFavoriteMake () {
   3. Create some instance properties that are specific to the specific
         car model (e.g. horsepower, cargo capacity, etc.)
 */
-function YourFavoriteModel () {
+function Civic () {
+  this.horsepower = "3 clydesdale's worth"
+  this.cargo = false
+  this.capacity = 1
 
 }
 // Set the prototype to appropriate model you created above and set the model name argument
-YourFavoriteModel.prototype = new ???;
+Civic.prototype = new Honda();
 
 /*
     Being an avid car collector, you own three different cars all of
@@ -38,9 +43,13 @@ YourFavoriteModel.prototype = new ???;
     2. Create a new property on each object to hold the license plate
        number for each car.
 */
-var myFirstFavoriteCar = new ???;
-myFirstFavoriteCar.plate_number = "";
-...
+var myFirstFavoriteCar = new Civic();
+var mySecondFavoriteCar = new Civic();
+var myThirdFavoriteCar = new Civic();
+myFirstFavoriteCar.plate_number = "notMINE";
+mySecondFavoriteCar.plate_number = "HI__MOM";
+myThirdFavoriteCar.plate_number = "lastOne";
+
 
 
 /*
@@ -48,17 +57,25 @@ myFirstFavoriteCar.plate_number = "";
   another model that's made by the manufacturer as your previous car. So if
   your first model was Fusion, make another one here for F150, for example.
  */
-function AnotherModel () {
+function Accord () {
 
 }
 /*
   Assign the prototype to the make you define above. Now both models will
   inherit the `manufacturer` property from the make function.
  */
-
+Accord.prototype = new Honda()
 
 
 /*
   Now create an instance of this new car model to put in your garage. Remember
   to give it a plate number.
  */
+var myFourthFavoriteCar = new Accord()
+myFourthFavoriteCar.plate_number = "_Su'in_"
+
+
+console.log(myFirstFavoriteCar);
+console.log(mySecondFavoriteCar);
+console.log(myThirdFavoriteCar);
+console.log(myFourthFavoriteCar);
